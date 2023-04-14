b8880da2adc139059a367c61e46f9ea2ad69b200
Chance Smith <chance.smith@lifeway.com>
2023-04-14 04:44:53 -0500

    ✅ TEST: add/edit tests (no prod code): [cli] --learn

--- src/__tests__/cli.e2e.test.ts
+++ src/__tests__/cli.e2e.test.ts
@@ -1,16 +1,32 @@
-import { $ } from "execa";
+import { $, execa } from "execa";
+import os from "os";
+import fs from "fs";
 import path from "path";
 
 // project-root/dist/cli.js
 const cliPath = path.join(__dirname, "..", "..", "dist", "cli.js");
 
+// async function setupTempGitRepo() {
+//   const tempDir = await fs.promises.mkdtemp(
+//     path.join(os.tmpdir(), "git-emoji-commit-test-")
+//   );
+
+//   await execa("git", ["init"], { cwd: tempDir });
+//   await execa("git", ["config", "user.name", "Test User"], { cwd: tempDir });
+//   await execa("git", ["config", "user.email", "test@example.com"], {
+//     cwd: tempDir,
+//   });
+
+//   return tempDir;
+// }
+
 describe("CLI E2E tests", () => {
-  test("cli.js should display a help message when run without arguments", async () => {
+  it("cli.js should display a help message when run without arguments", async () => {
     const { stdout } = await $`node ${cliPath} --help`;
     expect(stdout).toMatch(/Usage:/);
   });
 
-  test("cli.js should display the version number when run with --version or -V argument", async () => {
+  it("cli.js should display the version number when run with --version or -V argument", async () => {
     const { stdout } = await $`node ${cliPath} --version`;
     expect(stdout).toMatch(/^\d+\.\d+\.\d+(-[a-zA-Z0-9.]+)?$/);
 
@@ -18,5 +34,15 @@ describe("CLI E2E tests", () => {
     expect(stdout2).toMatch(/^\d+\.\d+\.\d+(-[a-zA-Z0-9.]+)?$/);
   });
 
-  // Add more test cases...
+  //   it("should warn user there are no staged files", async () => {
+  //     const tempRepoPath = await setupTempGitRepo();
+
+  //     const { stdout } = await execa(cliPath, [], { cwd: tempRepoPath });
+  //     expect(stdout).toMatch(/No staged files to commit/);
+  //   });
+
+  it("should show learn details", async () => {
+    const { stdout } = await $`node ${cliPath} --learn`;
+    expect(stdout).toMatch(/Learn more about/);
+  });
 });
